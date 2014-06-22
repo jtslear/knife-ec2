fbac97d0cf64dea63daed85f196c8ebfa9126fbc
Jason Draper <jason@drapergeek.com>
2014-06-22 14:48:33 -0400

    Add the options in

--- lib/chef/knife/ec2_volume_create.rb
+++ lib/chef/knife/ec2_volume_create.rb
@@ -43,24 +43,28 @@ class Chef
         :description => "The Availability Zone",
         :proc => Proc.new { |key| Chef::Config[:knife][:availability_zone] = key }
 
-      option :ebs_size,
-        :long => "--ebs-size SIZE",
-        :description => "The size of the EBS volume in GB, for EBS-backed instances"
+      option :volume_size,
+        :short => "-s",
+        :long => "--size SIZE",
+        :description => "The size of the EBS volume in GB",
+        :proc => Proc.new { |size| Chef::Config[:knife][:volume_size] = size }
 
       def run
         $stdout.sync = true
 
         validate!
 
-        @volume = connection.create_volume('us-east-1a', 5)
+        availability_zone = locate_config_value(:availability_zone)
+        volume_size = locate_config_value(:volume_size)
+        @volume = connection.create_volume(availability_zone, volume_size)
 
         msg_pair("Availability Zone", @volume.data[:body]["availabilityZone"])
-        puts "Done"
+        msg_pair("Volume Size", @volume.data[:body]["size"])
       end
 
 
       def validate!
-        super([:aws_access_key_id, :aws_secret_access_key])
+        super([:aws_access_key_id, :aws_secret_access_key, :availability_zone, :volume_size])
       end
     end
   end
